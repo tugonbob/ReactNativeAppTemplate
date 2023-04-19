5304142ee837c7c30a7a4233dd6de73fe4caa868
Joshua Gao <joshuakgao@gmail.com>
2023-04-19 17:53:09 -0500

    Removed unused imports, HomeScreen clean up

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,7 +1,6 @@
 import { BoxButton, H1, P1, ScrollableView } from "components";
-import { auth } from "../firebaseConfig";
 import React from "react";
-import { Text, View } from "react-native";
+import { auth } from "../firebaseConfig";
 
 export function HomeScreen() {
   return (
